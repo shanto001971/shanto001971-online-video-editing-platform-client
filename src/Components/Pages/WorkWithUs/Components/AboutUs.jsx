ba1e6fb80af5ccdeacbfa7d625fb80743285eebf
DiljahanToyshi <diljahantoyshi@gmail.com>
2023-09-11 21:38:14 +0600

    add the aboutUs and make it responsive

--- src/Components/Pages/WorkWithUs/Components/AboutUs.jsx
+++ src/Components/Pages/WorkWithUs/Components/AboutUs.jsx
@@ -6,22 +6,21 @@ const AboutUs = () => {
   
   return (
     <>
-      <div className=" mb-20">
-        <div className="  w-full  ">
+      <div className="  w-full  ">
         <div
             
-              className=" my-3 md:my-16 shadow-md shadow-gray-600 rounded-lg md:flex items-center justify-center gap-9 max-h[285px] mx-2 md:mx-20"
+              className=" my-3 md:my-10 lg:my-20 shadow-md  rounded-lg md:flex items-center justify-center gap-12  max-h[285px] mx-2 lg:mx-16"
             >
               <div className=" ">
               
                   <img
                     src={img1}
                     alt=""
-                    className="object-fill rounded-md w-[350px] md:w-[505px]"
+                    className="object-fill rounded-md w-[350px] md:w-[505px] lg:w-[600px]"
                   />
                
               </div>
-              <div className="mx-4 md:mx-0 w-[350px] md:w-[505px]">
+              <div className="mx-4 md:mx-0 w-[350px] md:w-[505px] lg:w-[600px]">
                 <p className="text-3xl font-bold md:w-4/6">Who we are  </p>
                 <p className="text-xl font-medium text-slate-800 py-2 md:py-5">
                   MotionMingle  is the online video editor that empowers anyone to tell stories worth sharing. We're used by millions of everyday editors worldwide. Our team innovates every day by developing exciting new tools, features and support systems for our users.
@@ -33,7 +32,7 @@ const AboutUs = () => {
             </div>
         <div
             
-              className="my-16 shadow-md shadow-gray-600 rounded-lg md:flex flex-row-reverse items-center justify-center gap-9 max-h[285px] mx-2 md:mx-20"
+              className="my-3 md:my-10 lg:my-20 shadow-md  rounded-lg md:flex flex-row-reverse items-center justify-center gap-9 max-h[285px] mx-2 lg:mx-20"
             >
               <div className=" ">
               <video
@@ -41,13 +40,13 @@ loop
 autoPlay
 playsInline
 muted
-className=" rounded-md  w-[350px] md:w-[505px]"
+className=" rounded-md  w-[350px] md:w-[505px] lg:w-[600px] "
 src='//videos.ctfassets.net/b4k16c7lw5ut/5OwShFGWt1HZJ2rCV3HDUL/9589889ccda34464c955cd99487ca22d/CC_Brand_Value_Slides_v7_optim.mp4'
 type="video/mp4"
 ></video>
                
               </div>
-              <div className="mx-4 md:mx-0 w-[350px] md:w-[505px]">
+              <div className="mx-4 md:mx-0 w-[350px] md:w-[505px] lg:w-[600px]">
                 <p className="text-3xl font-bold md:w-4/6">Our company values </p>
                 <p className="text-xl font-medium text-slate-800 py-2 md:py-5">
                 Words to work by. Our company values sit at the core of what MotionMingle is as a company.  We aim to produce innovative products, be an inspiring industry leader and facilitate a workplace people genuinely love.
@@ -59,18 +58,18 @@ type="video/mp4"
             </div>
             <div
             
-            className="my-16 shadow-md shadow-gray-600 rounded-lg md:flex items-center justify-center gap-9 max-h[285px]  mx-2 md:mx-20"
+            className="my-3 md:my-10 lg:my-20 shadow-md  rounded-lg md:flex items-center justify-center gap-9 max-h[285px]  mx-2 lg:mx-20"
           >
             <div className=" ">
             
                 <img
                   src={img2}
                   alt=""
-                  className="object-fill rounded-md w-[350px] md:w-[505px]"
+                  className="object-fill rounded-md w-[350px] md:w-[505px] lg:w-[600px]"
                 />
              
             </div>
-            <div className="mx-4 md:mx-0 w-[350px] md:w-[505px]">
+            <div className="mx-4 md:mx-0 w-[350px] md:w-[505px] lg:w-[600px]">
               <p className="text-3xl font-bold md:w-4/6">Who you can do </p>
               <p className="text-xl font-medium text-slate-800 py-2 md:py-5">
                 MotionMingle  is the online video editor that empowers anyone to tell stories worth sharing. We're used by millions of everyday editors worldwide. Our team innovates every day by developing exciting new tools, features and support systems for our users.
@@ -81,7 +80,6 @@ type="video/mp4"
             </div>
           </div>
         </div>
-      </div>
     </>
   );
 };
--- src/Components/Pages/WorkWithUs/WorkWithUS.jsx
+++ src/Components/Pages/WorkWithUs/WorkWithUS.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import VedioPart from './Components/VedioPart';
 import AboutUs from './Components/AboutUs';
+import WorksPerk from './Components/WorksPerk';
 
 const WorkWithUS = () => {
     return (
         <div>
      <VedioPart/>
      <AboutUs/>
+     <WorksPerk/>
         </div>
     );
 };
