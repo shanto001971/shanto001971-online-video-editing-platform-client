d47c93d5824eb8fd7e3e1d12c96040dd37e3e2a8
DiljahanToyshi <diljahantoyshi@gmail.com>
2023-09-11 23:38:19 +0600

    change the scrollsection

--- src/Components/Pages/WorkWithUs/WorkWithUS.jsx
+++ src/Components/Pages/WorkWithUs/WorkWithUS.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import VedioPart from './Components/VedioPart';
 import AboutUs from './Components/AboutUs';
 import WorksPerk from './Components/WorksPerk';
+import MissionVission from './Components/MissionVission';
 
 const WorkWithUS = () => {
     return (
         <div>
      <VedioPart/>
+     <MissionVission/>
      <AboutUs/>
      <WorksPerk/>
         </div>
