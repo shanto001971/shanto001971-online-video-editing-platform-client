0edde7f01503fe455921f15135a2123446dec166
chayansrk64 <chayansrk64@gmail.com>
2023-09-13 20:48:24 +0600

    chart added

--- src/Components/Pages/UserAdminDashboard/AdminHome/AdminHome.jsx
+++ src/Components/Pages/UserAdminDashboard/AdminHome/AdminHome.jsx
@@ -2,8 +2,7 @@ import { useContext } from "react";
 import { AuthContext } from "../../../../providers/AuthProvider";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
-
+    
 const AdminHome = () => {
     const {user} = useContext(AuthContext);
     const [axiosSecure] = useAxiosSecure();
@@ -17,52 +16,17 @@ const AdminHome = () => {
         }
     })
 
-  
+    const {data: chartData = []} = useQuery({
+        queryKey: ['admin-chart'],
+        queryFn: async() => {
+            const res = await axiosSecure('/admin-chart')
+            console.log(res)
+            return res.data;
+        }
+    })
 
-    // for charts
-    const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+    
 
-    const data = [
-        {
-          name: 'Revinue',
-          uv: 4000,
-          pv: 2400,
-          amt: 2400,
-        },
-        {
-          name: 'Users',
-          uv: 3000,
-          pv: 1398,
-          amt: 2210,
-        },
-        {
-          name: 'Videos',
-          uv: 2000,
-          pv: 9800,
-          amt: 2290,
-        },
-        {
-          name: 'Images',
-          uv: 2780,
-          pv: 3908,
-          amt: 2000,
-        },
-        
-      ];
-
-    const getPath = (x, y, width, height) => {
-        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
-        ${x + width / 2}, ${y}
-        C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
-        Z`;
-      };
-      
-      const TriangleBar = (props) => {
-        const { fill, x, y, width, height } = props;
-      
-        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
-      };
-      
 
     return (
         <div className="w-full h-full">
@@ -72,7 +36,7 @@ const AdminHome = () => {
   
                 <div className="stat flex-1">
                     <div className="stat-title text-white text-xl">Revinue</div>
-                    <div className="stat-value">$400</div>
+                    <div className="stat-value">$439</div>
                     <div className="stat-actions">
                     <div className="stat-desc text-primary">↗︎ 40 (2%)</div>
                     </div>
@@ -106,31 +70,12 @@ const AdminHome = () => {
             {/* charts */}
             <div className="flex  py-10">
                 <div className="w-1/2">
-                <BarChart
-                    width={500}
-                    height={300}
-                    data={data}
-                    margin={{
-                        top: 20,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                    >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Bar dataKey="uv" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
-                        {data.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-                        ))}
-                    </Bar>
-                </BarChart>
+                {/* Charts will appear here============== */}
                 </div>
 
-
+                {/* Pie chart */}
                 <div className="w-1/2">
-                    chart 2
+                 {/* another chart will appear here============ */}
                 </div>
             </div>
         </div>
