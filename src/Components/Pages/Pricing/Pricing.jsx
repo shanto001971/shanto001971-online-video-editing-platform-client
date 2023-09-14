4ad947001e3d633b81190a4e5146834ee9cbf391
TobiborRahman <r.tobibor16@gmail.com>
2023-09-14 12:41:07 +0600

    payment system implemented successfully

--- src/Components/Pages/Pricing/Pricing.jsx
+++ src/Components/Pages/Pricing/Pricing.jsx
@@ -1,27 +1,167 @@
-import './Pricing.css'
-import PricingTab from './PricingTab';
-
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const Pricing = () => {
-    return (
-        <div className='mb-10'>
-            <div className='flex justify-center  '>
-                <div className='BG p-10 mb-10 cursor-pointer'>
-                    <div> <p className='corner p-0'>Limited offer</p></div>
-                    <h3 className="text-center font-bold text-3xl">Lifetime plans for MingleMotion Express</h3>
-                    <div className="flex justify-center gap-8 mt-10 ">
-                        <div className="bg text-xl">Business <span className='font-bold'>$399</span> / <span className='text-sm'>billed once</span></div>
-                        <div className="bg text-xl">Unlimited <span className='font-bold'>$1099</span> / <span className='text-sm'>billed once</span> </div>
-                    </div>
-                    <div className='bgP text-white mt-6 flex justify-center mx-auto items-center '>Get lifetime plan</div>
-                    <div className='flex justify-end mt-3 font-semibold'>*Talk to our live chat support team to grab this deal today</div>
-                </div>
-            </div>
+	const price = {
+		pro: 7,
+		business: 15,
+	};
+	const btnClicked = (price) => {
+		fetch('http://localhost:5000/payments', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ price }),
+		})
+			.then((response) => response.json())
+			.then((result) => {
+				window.location.replace(result.url);
+				console.log(result);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	};
 
-            <div><h3 className='text-center font-semibold text-2xl'>The right plans,for the right price</h3></div>
-            <PricingTab />
-        </div >
-    );
+	return (
+		<div className="md:flex items-center justify-center gap-5 my-10">
+			<div className="card w-96 shadow-lg border">
+				<div className="card-body text-center">
+					<h2 className="card-title text-[#ff365b] text-2xl">Free</h2>
+					<h2 className="relative">
+						<span className="absolute left-24 top-[10px] font-semibold text-xl">
+							$
+						</span>{' '}
+						<span className="text-7xl">0</span>{' '}
+						<span> / month</span>
+					</h2>
+					<div className="text-left">
+						<p>
+							<span className="text-[#ff365b]">&#10003;</span>{' '}
+							Export 10 minutes of videos/month
+						</p>
+						<p>
+							<span className="text-[#ff365b]">&#10003;</span>{' '}
+							Export 720p videos at super speed
+						</p>
+						<p>
+							<span className="text-[#ff365b]">&#10003;</span> 2
+							GB of Cloud Storage
+						</p>
+						<p>
+							<span className="text-[#ff365b]">&#10003;</span>{' '}
+							Unlimited Collaborators
+						</p>
+						<p>
+							<span className="text-[#ff365b]">&#10003;</span>{' '}
+							Limited Transitions, Motions and Graphics library
+						</p>
+						<p>
+							<span className="text-[#ff365b]">&#10003;</span> 3
+							day Project and Media back-up
+						</p>
+					</div>
+
+					<div className="card-actions justify-center">
+						<button className="rounded-[4px] p-3 font-semibold text-white w-full bg-gradient-to-r from-[#ff365b] to-[#ff9436]">
+							<Link to="/dashboard">Go Free</Link>
+						</button>
+					</div>
+				</div>
+			</div>
+			<div className="card w-96 shadow-lg border">
+				<div className="card-body text-center">
+					<h2 className="card-title text-[#00d4ff] text-2xl">Pro</h2>
+					<h2 className="relative">
+						<span className="absolute left-24 top-[10px] font-semibold text-xl">
+							$
+						</span>{' '}
+						<span className="text-7xl">7</span>{' '}
+						<span> / month</span>
+					</h2>
+					<div className="text-left">
+						<p>
+							<span>&#10003;</span> Export 10 minutes of
+							videos/month
+						</p>
+						<p>
+							<span>&#10003;</span> Export 720p videos at super
+							speed
+						</p>
+						<p>
+							<span>&#10003;</span> 2 GB of Cloud Storage
+						</p>
+						<p>
+							<span>&#10003;</span> Unlimited Collaborators
+						</p>
+						<p>
+							<span>&#10003;</span> Limited Transitions, Motions
+							and Graphics library
+						</p>
+						<p>
+							<span>&#10003;</span> 3 day Project and Media
+							back-up
+						</p>
+					</div>
+
+					<div className="card-actions justify-center">
+						<button
+							onClick={() => btnClicked(price.pro)}
+							className="rounded-[4px] p-3 font-semibold text-white w-full bg-gradient-to-r from-[#00d4ff] to-[#0062ff]"
+						>
+							Go Pro
+						</button>
+					</div>
+				</div>
+			</div>
+			<div className="card w-96 shadow-lg border gradient-border">
+				<div className="card-body text-center">
+					<h2 className="card-title text-[#de2898] text-2xl">
+						Business
+					</h2>
+					<h2 className="relative">
+						<span className="absolute left-20 font-semibold top-[10px] text-xl">
+							$
+						</span>{' '}
+						<span className="text-7xl">15</span>{' '}
+						<span> / month</span>
+					</h2>
+					<div className="text-left">
+						<p>
+							<span>&#10003;</span> Export 10 minutes of
+							videos/month
+						</p>
+						<p>
+							<span>&#10003;</span> Export 720p videos at super
+							speed
+						</p>
+						<p>
+							<span>&#10003;</span> 2 GB of Cloud Storage
+						</p>
+						<p>
+							<span>&#10003;</span> Unlimited Collaborators
+						</p>
+						<p>
+							<span>&#10003;</span> Limited Transitions, Motions
+							and Graphics library
+						</p>
+						<p>
+							<span>&#10003;</span> 3 day Project and Media
+							back-up
+						</p>
+					</div>
+
+					<div className="card-actions justify-center">
+						<button
+							onClick={() => btnClicked(price.business)}
+							className="rounded-[4px] p-3 font-semibold text-white w-full bg-gradient-to-r from-[#de2898] to-[#9166ff]"
+						>
+							Go Business
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Pricing;
--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,136 +1,137 @@
-import { createBrowserRouter } from "react-router-dom";
-import LayOut from "../Components/LayOut/LayOut";
-import Home from "../Components/Home/Home";
-import Login from "../Components/Pages/Login/Login";
-import Register from "../Components/Pages/Register/Register";
+import { createBrowserRouter } from 'react-router-dom';
+import LayOut from '../Components/LayOut/LayOut';
+import Home from '../Components/Home/Home';
+import Login from '../Components/Pages/Login/Login';
+import Register from '../Components/Pages/Register/Register';
 
-import OnlineVideoEditor from "../Components/Pages/OnlineVideoEditor/OnlineVideoEditor";
-import About from "../Components/Pages/About/About";
-import DesktopVideo from "../Components/Pages/DesktopVideo/DesktopVideo/DesktopVideo";
-import MobileVideo from "../Components/Pages/MobileVideo/MobileVideo/MobileVideo";
+import OnlineVideoEditor from '../Components/Pages/OnlineVideoEditor/OnlineVideoEditor';
+import About from '../Components/Pages/About/About';
+import DesktopVideo from '../Components/Pages/DesktopVideo/DesktopVideo/DesktopVideo';
+import MobileVideo from '../Components/Pages/MobileVideo/MobileVideo/MobileVideo';
 // import ErrorComponent from "../Components/Pages/Error/ErrorComponent";
-import VideoEditeLayout from "../Components/LayOut/Dashboard/videoEditePage/VideoEditeLayout/VideoEditeLayout";
-import VideoEditePage from "../Components/LayOut/Dashboard/videoEditePage/videoEditePage/VideoEditePage";
-import UserAdminDashboard from "../Components/LayOut/UserAdminDashboard";
-import AllUsers from "../Components/Pages/UserAdminDashboard/AllUsers/AllUsers";
-import UsersChart from "../Components/Pages/UserAdminDashboard/UsersChart/UsersChart";
-import HelpEmail from "../Components/Pages/Help/HelpEmail";
-import LearnEducationalVideo from "../Components/Pages/Learn/LearnEducationalVideo/LearnEducationalVideo";
-import LearnExplainerVideo from "../Components/Pages/Learn/LearnExplainerVideo/LearnExplainerVideo";
-import LearnTrainingVideo from "../Components/Pages/Learn/LearnTrainingVideo/LearnTrainingVideo";
-import Templates from "../Components/Pages/Templates/TemplatesForMobile/Templates";
-import AllTemplates from "../Components/Pages/Templates/AllTemplates/AllTemplates";
-import Pricing from "../Components/Pages/Pricing/Pricing";
-import UserProfile from "../Components/Pages/UserProfilePage/UserProfilePage";
-import WorkWithUS from "../Components/Pages/WorkWithUs/WorkWithUS";
-import AdminChart from "../Components/Pages/UserAdminDashboard/AdminChart/AdminChart";
-
-
+import VideoEditeLayout from '../Components/LayOut/Dashboard/videoEditePage/VideoEditeLayout/VideoEditeLayout';
+import VideoEditePage from '../Components/LayOut/Dashboard/videoEditePage/videoEditePage/VideoEditePage';
+import UserAdminDashboard from '../Components/LayOut/UserAdminDashboard';
+import AllUsers from '../Components/Pages/UserAdminDashboard/AllUsers/AllUsers';
+import UsersChart from '../Components/Pages/UserAdminDashboard/UsersChart/UsersChart';
+import HelpEmail from '../Components/Pages/Help/HelpEmail';
+import LearnEducationalVideo from '../Components/Pages/Learn/LearnEducationalVideo/LearnEducationalVideo';
+import LearnExplainerVideo from '../Components/Pages/Learn/LearnExplainerVideo/LearnExplainerVideo';
+import LearnTrainingVideo from '../Components/Pages/Learn/LearnTrainingVideo/LearnTrainingVideo';
+import Templates from '../Components/Pages/Templates/TemplatesForMobile/Templates';
+import AllTemplates from '../Components/Pages/Templates/AllTemplates/AllTemplates';
+import Pricing from '../Components/Pages/Pricing/Pricing';
+import UserProfile from '../Components/Pages/UserProfilePage/UserProfilePage';
+import WorkWithUS from '../Components/Pages/WorkWithUs/WorkWithUS';
+import AdminChart from '../Components/Pages/UserAdminDashboard/AdminChart/AdminChart';
+import PaymentSuccess from '../Components/Pages/Pricing/PaymentSuccess';
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <LayOut />,
-        // errorElement:<ErrorComponent/>,
-        children: [
-            {
-                path: "/",
-                element: <Home />
-            },
-            {
-                path: "/emailUs",
-                element: <HelpEmail />
-            },
+	{
+		path: '/',
+		element: <LayOut />,
+		// errorElement:<ErrorComponent/>,
+		children: [
+			{
+				path: '/',
+				element: <Home />,
+			},
+			{
+				path: '/emailUs',
+				element: <HelpEmail />,
+			},
 
-            {
-                path: "/templates-for-mobile",
-                element: <Templates />
-            },
-            {
-                path: "/all-templates",
-                element: <AllTemplates />
-            },
-            {
-                path: '/login',
-                element: <Login></Login>
-            },
-            {
-                path: '/about',
-                element: <About></About>
-            },
-            {
-                path: '/register',
-                element: <Register></Register>
-            },
-            {
-                path: '/onlinevideo',
-                element: <OnlineVideoEditor></OnlineVideoEditor>
-            },
-            {
-                path: "/desktopvideo",
-                element: <DesktopVideo></DesktopVideo>
-            },
-            {
-                path: "/mobilevideo",
-                element: <MobileVideo></MobileVideo>
-            },
-            {
-                path: "/work-with-us",
-                element: <WorkWithUS/>
-            },
+			{
+				path: '/templates-for-mobile',
+				element: <Templates />,
+			},
+			{
+				path: '/all-templates',
+				element: <AllTemplates />,
+			},
+			{
+				path: '/login',
+				element: <Login></Login>,
+			},
+			{
+				path: '/about',
+				element: <About></About>,
+			},
+			{
+				path: '/register',
+				element: <Register></Register>,
+			},
+			{
+				path: '/onlinevideo',
+				element: <OnlineVideoEditor></OnlineVideoEditor>,
+			},
+			{
+				path: '/desktopvideo',
+				element: <DesktopVideo></DesktopVideo>,
+			},
+			{
+				path: '/mobilevideo',
+				element: <MobileVideo></MobileVideo>,
+			},
+			{
+				path: '/work-with-us',
+				element: <WorkWithUS />,
+			},
 
-            {
-                path: "/learn-educational-video",
-                element: <LearnEducationalVideo />
-            },
-            {
-                path: "/learn-explainer-video",
-                element: <LearnExplainerVideo />
-            },
-            {
-                path: "/learn-training-video",
-                element: <LearnTrainingVideo />
-            },
-            {
-                path: "/pricing",
-                element: <Pricing />
-            },
-            {
-                path: "/user-profile",
-                element: <UserProfile />
-            },
-        ]
-    },
-    {
-        path: "/dashboard",
-        element: <VideoEditeLayout />,
-        children: [
-            {
-                path: "/dashboard",
-                element: <VideoEditePage />
-            }
-        ]
+			{
+				path: '/learn-educational-video',
+				element: <LearnEducationalVideo />,
+			},
+			{
+				path: '/learn-explainer-video',
+				element: <LearnExplainerVideo />,
+			},
+			{
+				path: '/learn-training-video',
+				element: <LearnTrainingVideo />,
+			},
+			{
+				path: '/pricing',
+				element: <Pricing />,
+			},
+			{
+				path: '/user-profile',
+				element: <UserProfile />,
+			},
+			{
+				path: '/payments/success/:tran_id',
+				element: <PaymentSuccess />,
+			},
+		],
+	},
+	{
+		path: '/dashboard',
+		element: <VideoEditeLayout />,
+		children: [
+			{
+				path: '/dashboard',
+				element: <VideoEditePage />,
+			},
+		],
+	},
 
-    },
-
-    {
-        path: "useradmindashboard",
-        element: <UserAdminDashboard></UserAdminDashboard>,
-        children: [
-            {
-                path: "allusers",
-                element: <AllUsers></AllUsers>
-            },
-            {
-                path: "userschart",
-                element: <UsersChart></UsersChart>
-            },
-            // admin routes
-            {
-                path: "adminchart",
-                element: <AdminChart></AdminChart>
-            }
-        ]
-    }
-
+	{
+		path: 'useradmindashboard',
+		element: <UserAdminDashboard></UserAdminDashboard>,
+		children: [
+			{
+				path: 'allusers',
+				element: <AllUsers></AllUsers>,
+			},
+			{
+				path: 'userschart',
+				element: <UsersChart></UsersChart>,
+			},
+			// admin routes
+			{
+				path: 'adminchart',
+				element: <AdminChart></AdminChart>,
+			},
+		],
+	},
 ]);
