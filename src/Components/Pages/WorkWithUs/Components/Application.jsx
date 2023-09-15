2b702b495c1a8c03b6ac4e7c53de32811f8bfd9f
DiljahanToyshi <diljahantoyshi@gmail.com>
2023-09-15 19:02:59 +0600

    add some margin in work with us

--- src/Components/Pages/WorkWithUs/Components/Application.jsx
+++ src/Components/Pages/WorkWithUs/Components/Application.jsx
@@ -32,7 +32,7 @@ const Application = () => {
   };
 
   return (
-    <div className="max-w-[307px]md:max-w-[500px] text-center mx-3 md:mx-9 my-8 md:my-14 ">
+    <div className="max-w-[307px] md:max-w-[500px] text-center mx-3 md:mx-9 my-8 md:my-14 ">
       <h1 className="text-3xl font-medium leading-none py-2 ">
         General Application
       </h1>
@@ -121,7 +121,7 @@ const Application = () => {
                     <input
                       type="number"
                       {...register("contactNo", { required: true })}
-                      className=" w-full md:px-3 md:py-2 border rounded-md focus:outline-none border-indigo-300 focus:border-indigo-500 bg-gray-100 text-gray-900 "
+                      className=" w-full md:px-3 md:py-2 -mt-[2px] border rounded-md focus:outline-none border-indigo-300 focus:border-indigo-500 bg-gray-100 text-gray-900 "
                     />
                   </div>
                 </div>
