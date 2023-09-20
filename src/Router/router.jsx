d65dafe0742af02db50c92a7b21fcef24419fbea
Shanto Islam <rajs45693@gmail.com>
2023-09-20 22:29:12 +0600

    slove some error issu

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -110,14 +110,6 @@ export const router = createBrowserRouter([
                 element: <VideoEditePage />,
             },
         ],
-        path: "/dashboard",
-        element: <VideoEditeLayout />,
-        children: [
-            {
-                path: "/dashboard",
-                element: <VideoEditePage />
-            }
-        ]
 
     },
 
