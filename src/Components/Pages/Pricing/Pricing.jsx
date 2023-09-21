597e644be93db548fa36ede2bb042db1f1ea0928
TobiborRahman <r.tobibor16@gmail.com>
2023-09-22 01:15:31 +0600

    pricing section responsive done

--- src/Components/Pages/Pricing/Pricing.jsx
+++ src/Components/Pages/Pricing/Pricing.jsx
@@ -36,38 +36,38 @@ const Pricing = () => {
 				<title>MingleMotion Express | Pricing</title>
 			</Helmet>
 			<div
-				className={`text-center my-14 font-bg-blue-800 font ${
+				className={`text-center my-10 font-bg-blue-800 ${
 					theme.mode === 'dark' ? 'text-gray-100' : 'text-black'
 				}`}
 			>
-				<h1 className="text-3xl">
+				<h1 className="text-3xl rubik font-semibold">
 					<span className="text-[#009bff]">10x faster</span> than any
 					other video tool
 				</h1>
-				<h1 className="text-3xl">
+				<h1 className="text-3xl rubik font-semibold">
 					<span className="text-[#de2898]">100x cheaper</span> than
 					buying a dedicated video editing computer
 				</h1>
 			</div>
 
 			<div
-				className={`md:flex items-center justify-center gap-5 my-10 mx-10 font ${
+				className={`md:flex items-center justify-center gap-5 my-10 md:mx-10 ${
 					theme.mode === 'dark' ? 'text-gray-100' : 'text-black'
 				}`}
 			>
 				<div className="card w-96 shadow-lg border hover:border-[#ff365b] duration-300">
 					<div className="card-body text-center">
-						<h2 className="card-title text-[#ff365b] text-2xl">
+						<h2 className="card-title text-[#ff365b] text-2xl rubik">
 							Free
 						</h2>
-						<h2 className="relative">
-							<span className="absolute left-24 top-[10px] font-semibold text-xl">
+						<h2 className="relative poppins">
+							<span className="absolute left-[89px] top-[3px] font-semibold text-xl">
 								$
 							</span>{' '}
 							<span className="text-6xl">0</span>{' '}
 							<span> / month</span>
 						</h2>
-						<div className="text-left">
+						<div className="text-left leading-relaxed">
 							<p>
 								<span className="text-[#ff365b]">&#10003;</span>{' '}
 								Export 10 minutes of videos/month
@@ -102,40 +102,43 @@ const Pricing = () => {
 						</div>
 					</div>
 				</div>
-				<div className="card w-96 shadow-lg border gradient-border2 duration-700">
+				<div className="card w-96 shadow-lg border hover:border-[#00d4ff] duration-300">
 					<div className="card-body text-center">
 						<h2 className="card-title text-[#00d4ff] text-2xl">
 							Pro
 						</h2>
 						<h2 className="relative">
-							<span className="absolute left-24 top-[10px] font-semibold text-xl">
+							<span className="absolute left-[89px] top-[3px] font-semibold text-xl">
 								$
 							</span>{' '}
 							<span className="text-6xl">7</span>{' '}
 							<span> / month</span>
 						</h2>
-						<div className="text-left">
+						<div className="text-left leading-relaxed">
 							<p>
-								<span>&#10003;</span> Export 10 minutes of
-								videos/month
+								<span className="text-[#00d4ff]">&#10003;</span>{' '}
+								Export 10 minutes of videos/month
 							</p>
 							<p>
-								<span>&#10003;</span> Export 720p videos at
-								super speed
+								<span className="text-[#00d4ff]">&#10003;</span>{' '}
+								Export 720p videos at super speed
 							</p>
 							<p>
-								<span>&#10003;</span> 2 GB of Cloud Storage
+								<span className="text-[#00d4ff]">&#10003;</span>{' '}
+								2 GB of Cloud Storage
 							</p>
 							<p>
-								<span>&#10003;</span> Unlimited Collaborators
+								<span className="text-[#00d4ff]">&#10003;</span>{' '}
+								Unlimited Collaborators
 							</p>
 							<p>
-								<span>&#10003;</span> Limited Transitions,
-								Motions and Graphics library
+								<span className="text-[#00d4ff]">&#10003;</span>{' '}
+								Limited Transitions, Motions and Graphics
+								library
 							</p>
 							<p>
-								<span>&#10003;</span> 3 day Project and Media
-								back-up
+								<span className="text-[#00d4ff]">&#10003;</span>{' '}
+								3 day Project and Media back-up
 							</p>
 						</div>
 
@@ -149,40 +152,43 @@ const Pricing = () => {
 						</div>
 					</div>
 				</div>
-				<div className="card w-96 shadow-lg border gradient-border3 duration-700">
+				<div className="card w-96 shadow-lg border hover:border-[#de2898] duration-300">
 					<div className="card-body text-center">
 						<h2 className="card-title text-[#de2898] text-2xl">
 							Business
 						</h2>
 						<h2 className="relative">
-							<span className="absolute left-20 font-semibold top-[10px] text-xl">
+							<span className="absolute left-[76px] top-[3px] font-semibold text-xl">
 								$
 							</span>{' '}
 							<span className="text-6xl">15</span>{' '}
 							<span> / month</span>
 						</h2>
-						<div className="text-left">
+						<div className="text-left leading-relaxed">
 							<p>
-								<span>&#10003;</span> Export 10 minutes of
-								videos/month
+								<span className="text-[#de2898]">&#10003;</span>{' '}
+								Export 10 minutes of videos/month
 							</p>
 							<p>
-								<span>&#10003;</span> Export 720p videos at
-								super speed
+								<span className="text-[#de2898]">&#10003;</span>{' '}
+								Export 720p videos at super speed
 							</p>
 							<p>
-								<span>&#10003;</span> 2 GB of Cloud Storage
+								<span className="text-[#de2898]">&#10003;</span>{' '}
+								2 GB of Cloud Storage
 							</p>
 							<p>
-								<span>&#10003;</span> Unlimited Collaborators
+								<span className="text-[#de2898]">&#10003;</span>{' '}
+								Unlimited Collaborators
 							</p>
 							<p>
-								<span>&#10003;</span> Limited Transitions,
-								Motions and Graphics library
+								<span className="text-[#de2898]">&#10003;</span>{' '}
+								Limited Transitions, Motions and Graphics
+								library
 							</p>
 							<p>
-								<span>&#10003;</span> 3 day Project and Media
-								back-up
+								<span className="text-[#de2898]">&#10003;</span>{' '}
+								3 day Project and Media back-up
 							</p>
 						</div>
 
@@ -199,11 +205,11 @@ const Pricing = () => {
 			</div>
 
 			<div
-				className={`flex justify-between items-center mx-10 mb-10 border px-16 rounded-lg ${
+				className={`md:flex justify-between items-center py-10 md:py-5 mx-10 mb-10 border  px-8 md:px-16 rounded-lg ${
 					theme.mode === 'dark' ? 'text-gray-100' : 'text-black'
 				}`}
 			>
-				<div className="font">
+				<div className="">
 					<h3 className="text-2xl font-semibold text-[#009bff] pb-3">
 						Enterprise
 					</h3>
@@ -214,14 +220,14 @@ const Pricing = () => {
 					</p>
 				</div>
 				<div>
-					<h2 className="relative font">
-						<span className="absolute -left-2 top-[10px] font-semibold text-xl">
+					<h2 className="relative mt-5 md:mt-0 text-left">
+						<span className="absolute left-[80px] top-[3px] font-semibold text-xl">
 							$
 						</span>{' '}
 						<span className="text-6xl">55</span>{' '}
 						<span> / month</span>
 					</h2>
-					<p className="font">Annual Billing Only</p>
+					<p>Annual Billing Only</p>
 					<button className="rounded-[4px] text-white font-bold my-3 bg-gradient-to-r from-[#00ced1] to-[#00d56e] py-[.8125rem] px-[2rem]">
 						Contact Us
 					</button>
